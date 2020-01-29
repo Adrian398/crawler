d214b7d4aece8a0f1b91ff885ed458d41f233151
Sametros <adrian@stellaris.de>
2020-01-29 10:57:49 +0100

    Clear Project structure

--- old_crawlers/Gruppe 3/CrawlSitesGroup3.R
+++
@@ -1,9 +0,0 @@
-source("CrawlAllSitesLukas.R")
-library(openxlsx)
-
-GesamtEvents <- data.frame(matrix(ncol = 14, nrow = 0))
-                           
-EventsLukas <- EventCrawlerLukas()
-GesamtEvents <- rbind(EventsLukas,GesamtEvents)
-
-write.xlsx(GesamtEvents, file = "EventsLukas.xlsx", asTable = TRUE)
